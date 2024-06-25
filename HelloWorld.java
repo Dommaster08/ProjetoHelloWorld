f091b8e6b46e83722662070a98e4a5b466e4bc07
Luan Feitosa <luancx7@gmail.com>
2024-06-25 16:46:27 -0300

    Primeiro commit: Adicionando projeto HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("---------------------------");
+        System.err.println("Unicesumar - Mapa Tópicos em Computação II");
+        System.out.println("Olá Mundo!");   
+        System.out.println("---------------------------");
+        System.out.println("Aluno: Luan Feitosa Silva de Carvalho");
+    }
+    
+}
